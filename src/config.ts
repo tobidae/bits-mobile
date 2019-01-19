fd821013b82e42799c2a6780ab260b9f2f64f489
Tobi Akerele <6331259+tobidae@users.noreply.github.com>
2019-01-19 18:19:10 -0500

    Delete config.ts

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD9vrUyCuC4T_uCcEWowOSnnxAUzRfMfPg",
-  authDomain: "case-manager-boeing.firebaseapp.com",
-  databaseURL: "https://case-manager-boeing.firebaseio.com",
-  projectId: "case-manager-boeing",
-  storageBucket: "case-manager-boeing.appspot.com",
-  messagingSenderId: "546333132486"
-};
